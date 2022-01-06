97eb4295e011f62c34059eff5089042cdf91b697
Jayakrishna <73790114+jayakrishna352@users.noreply.github.com>
2022-01-07 02:48:23 +0530

    Create FindMissingAndRepeating.cpp

---
+++ Goldman Sachs/FindMissingAndRepeating.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int *findTwoElement(int *arr, int n)
+    {
+
+        int xor1;
+
+        int set_bit_no;
+
+        int i;
+        int x = 0;
+        int y = 0;
+
+        xor1 = arr[0];
+
+        for (i = 1; i < n; i++)
+            xor1 = xor1 ^ arr[i];
+
+        for (i = 1; i <= n; i++)
+            xor1 = xor1 ^ i;
+
+        set_bit_no = xor1 & ~(xor1 - 1);
+
+        for (i = 0; i < n; i++)
+        {
+            if (arr[i] & set_bit_no)
+                x = x ^ arr[i];
+
+            else
+                y = y ^ arr[i];
+        }
+
+        for (i = 1; i <= n; i++)
+        {
+            if (i & set_bit_no)
+                x = x ^ i;
+
+            else
+                y = y ^ i;
+        }
+
+        int x_count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == x)
+                x_count++;
+        }
+        int *ans = new int[2];
+        ans[0] = x;
+        ans[1] = y;
+        if (x_count == 0)
+        {
+            ans[0] = y;
+            ans[1] = x;
+            return ans;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a, n);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
